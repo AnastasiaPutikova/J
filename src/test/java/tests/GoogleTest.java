b34869c03e956f4660fc50dc7116f8f3c6d69aaa
Anastasiya <a.putikova@qa-academy.by>
2022-05-16 20:48:32 +0300

    change parameter

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -11,7 +11,7 @@ public class GoogleTest {
     public void check() {
         WebDriverManager.chromedriver().setup();
         WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get(System.getProperty("url"));
+        chromeDriver.get(System.getProperty("URL"));
         assert true;
         chromeDriver.quit();
     }
