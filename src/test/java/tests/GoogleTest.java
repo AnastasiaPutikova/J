0e57e1142459038e1d777c69c782ffa4433ccd4e
Anastasiya <a.putikova@qa-academy.by>
2022-05-16 20:14:27 +0300

    add google check

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+    @Test
+    public void check() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://google.com");
+        chromeDriver.quit();
+    }
+}
